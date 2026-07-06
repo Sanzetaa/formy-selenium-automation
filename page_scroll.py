7a84209c369a42a8b4a380e146106556fe55d5c5
Sanjita Adhikari <sanzeta23@gmail.com>
2026-07-06 23:39:23 +0545

    feat: add test for page scroll

---
+++ page_scroll.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+import time
+
+
+
+def main_page(driver):
+    form_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@class='btn btn-lg'][normalize-space()='Page Scroll']"))
+    )
+    form_button.click()
+    assert "scroll" in driver.current_url
+    time.sleep(3)
+
+def page_scroll(driver):
+    page_height = driver.execute_script("return document.body.scrollHeight")
+    scroll_speed = 300
+    scroll_iteration=int(page_height/scroll_speed)
+
+    for _ in range(scroll_iteration):
+        driver.execute_script(f"window.scrollBy(0, {scroll_speed});")
+        time.sleep(3)
+
+    full_name=driver.find_element(By.ID, "name")
+    date=driver.find_element(By.ID, "date")
+
+    full_name.send_keys("Justin Beiber ")
+    date.send_keys("04/14/2026")
+    time.sleep(3)
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("https://formy-project.herokuapp.com/")
+    driver.maximize_window()
+    main_page(driver)
+    page_scroll(driver)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
