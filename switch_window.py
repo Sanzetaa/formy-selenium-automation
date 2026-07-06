8c4b9238af6016914869912a847d8a143f2d5d86
Sanjita Adhikari <sanzeta23@gmail.com>
2026-07-06 23:54:25 +0545

    feat: add test for switch window

---
+++ switch_window.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+import time
+
+
+
+def main_page(driver):
+    form_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@class='btn btn-lg'][normalize-space()='Switch Window']"))
+    )
+    form_button.click()
+    time.sleep(3)
+
+def open_new_tab(driver):
+    new_tab = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@id='new-tab-button']"))
+    )
+    new_tab.click()
+    time.sleep(3)
+
+    driver.switch_to.window(driver.window_handles[1])
+    time.sleep(3)
+
+
+
+def click_autocomplete(driver):
+    form_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@class='btn btn-lg'][normalize-space()='Autocomplete']"))
+    )
+    form_button.click()
+    time.sleep(3)
+
+
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("https://formy-project.herokuapp.com/")
+    driver.maximize_window()
+    main_page(driver)
+    open_new_tab(driver)
+    click_autocomplete(driver)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
